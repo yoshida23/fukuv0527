15f2afe75e269381c33a09145e790c9b4f1d578d
D301-06 <tnk.dat.class@gmail.com>
2021-05-27 09:40:41 +0900

    移動の復習

--- fukuv0527/Form1.cs
+++ fukuv0527/Form1.cs
@@ -17,5 +17,24 @@ namespace fukuv0527
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
     }
 }
